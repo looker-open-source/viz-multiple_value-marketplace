d089afc7575c955bcaeb4f7932b5534efaabe740
Minhaz Kazi <vlookup@google.com>
2026-06-04 16:50:28 +0000

    Fix OSPO compliance issues

--- src/constants/locale_formats_tags.js
+++ src/constants/locale_formats_tags.js
@@ -1,3 +1,8 @@
+// © 2019 Google LLC.  All rights reserved.
+//
+// This software is subject to the Google Cloud Terms of Service, as
+// modified by the "General Software Terms" of the Google Cloud Service Specific Terms, available at: https://cloud.google.com/terms/service-terms.
+
 export const LOCALE_NUMBER_FORMATS = {
   DEFAULT_EN: '1,234.56',
   DEFAULT_EU: '1.234,56',
